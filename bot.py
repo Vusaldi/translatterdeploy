404f1dc1b35e2c1e430ead3629503ac4bb449df3
Vusaldi <93197397+Vusaldi@users.noreply.github.com>
2025-06-22 09:04:41 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,302 +1,87 @@
-# @mrlokaman ©️ shyan
-#lntechnical
+from pyrogram import Client, filters
 from pyrogram.enums import ParseMode
-import os
-from pyrogram import Client, filters
-from pyrogram.types import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup
-)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from googletrans import Translator
+import os
+
 TOKEN = os.environ.get("TOKEN", "zzzzzzzz")
-
 API_ID = int(os.environ.get("API_ID", "8953338"))
-
 API_HASH = os.environ.get("API_HASH", "fe21f223cb02d8f7c1cbda651f553a45")
-app = Client(
-        "Gtt",
-        bot_token=TOKEN,api_hash=API_HASH,
-            api_id=API_ID
-    )
 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+app = Client("Gtt", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH)
 
-@app.on_message(filters.private & filters.command(['start']))
+# Başlanğıc mesajı
+@app.on_message(filters.private & filters.command("start"))
 async def start(client, message):
     await message.reply_text(
-        text=f"Salam **{message.from_user.first_name}** \n\n __Mən Tərcüməçi Bot 🌎 'Am\n Mən Göndərdiyin Mesajı Seçdiyin Dilə Tərcümə Etmək Üçün Proqramlaşdırılmışam\nBaşlamaq Üçün Mənə 1 Mesaj Göndər__",
-        reply_to_message_id=message.message_id,
+        f"Salam **{message.from_user.first_name}**\n\n"
+        "__Mən Tərcüməçi Botuyam 🌎\n"
+        "Göndərdiyin Mesajı Seçdiyin Dilə Tərcümə Etmək Üçün Proqramlaşdırılmışam.\n"
+        "Başlamaq Üçün Mənə 1 Mesaj Göndər.__",
         parse_mode=ParseMode.MARKDOWN,
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("🐾 Owner", url="t.me/vusalxw")],
-            [InlineKeyboardButton("Yenilik Kanalı 🌎", url="t.me/TercumeciBotYenilikler")]
+            [InlineKeyboardButton("🐾 Owner", url="https://t.me/vusalxw")],
+            [InlineKeyboardButton("Yenilik Kanalı 🌎", url="https://t.me/TercumeciBotYenilikler")]
         ])
     )
 
-                  
+# Əsas dillər klaviaturası
+language_pages = {
+    "page1": [
+        ["az", "de", "fr"],
+        ["ko", "en", "tr"],
+        ["el", "be", "ru"],
+        ["pt", "bg", "es"],
+        ["fa", "hr", "cs"],
+        ["da", "it", "uk"]
+    ],
+    "page2": [
+        ["hy", "et", "fi"],
+        ["am", "fy", "gl"],
+        ["ka", "sq", "eu"],  # eu = Bask dili
+        ["gu", "ht", "ha"],
+        ["hi", "hu", "is"],
+        ["ig", "id", "ga"]
+    ]
+    # Əlavə səhifələr buraya əlavə edilə bilər...
+}
 
+def get_language_keyboard(page):
+    buttons = []
+    for row in language_pages.get(page, []):
+        btn_row = [InlineKeyboardButton(lang.upper(), callback_data=lang) for lang in row]
+        buttons.append(btn_row)
+    buttons.append([
+        InlineKeyboardButton("««« Geri", callback_data="page1") if page != "page1" else InlineKeyboardButton(" ", callback_data="none"),
+        InlineKeyboardButton("Növbəti »»»", callback_data="page2" if page == "page1" else "page1")
+    ])
+    return InlineKeyboardMarkup(buttons)
 
+# İstifadəçi mətn göndərəndə dilləri göstər
+@app.on_message(filters.private & filters.text)
+async def ask_language(client, message):
+    await message.reply_text(
+        "👇 Dili Seçin:",
+        reply_to_message_id=message.message_id,
+        reply_markup=get_language_keyboard("page1")
+    )
 
-@app.on_message(filters.private & filters.text  )
-async def echo(client, message):
-
- 
- keybord1= InlineKeyboardMarkup( [
-        [ 
-            InlineKeyboardButton("Azərbaycanca ",callback_data = 'az'),
-             InlineKeyboardButton("Almanca", callback_data='de'),
-            InlineKeyboardButton("Fransızca",callback_data ='fr'),
-        ],
-        [   InlineKeyboardButton("Koreya dili", callback_data='ko'),
-        InlineKeyboardButton("İngilscə", callback_data='en'),      
-        InlineKeyboardButton("Türkcə",callback_data = 'tr'),        
-        ],
-        [InlineKeyboardButton("Yunanca",callback_data ="el"),
-        	 InlineKeyboardButton("Belarusca",callback_data ="be"),       	
-	InlineKeyboardButton("Rusca",callback_data="ru")],
-	
-	[InlineKeyboardButton("Portuqalca",callback_data = "pt"),
-	InlineKeyboardButton("Bolqarca",callback_data ="bg"),
-	InlineKeyboardButton("İspanca",callback_data = "es")
-	],
-	[ 
-	InlineKeyboardButton("Farsca",callback_data ="fa"),
-	InlineKeyboardButton("Xorvatca",callback_data = "hr"),
-	InlineKeyboardButton("Çexcə", callback_data = "cs"),
-	],
-	[ InlineKeyboardButton("Danimarkaca",callback_data = "da"),
-	InlineKeyboardButton("İtalyanca",callback_data = "it"),
-	InlineKeyboardButton("Ukraynaca",callback_data = "uk"),	 
-	],
-        [InlineKeyboardButton("☎️ Burada sizin reklamınız ola bilər ✔ " ,url="https://t.me/vusalxw")
-        ],
-	[InlineKeyboardButton(" Növbəti »»»",callback_data = "page2")
-	]
-	] )
-	
- await  message.reply_text("👇",reply_to_message_id = message.message_id, reply_markup = keybord1) 
-
-
+# Callback Query ilə dilləri dəyiş və ya tərcümə et
 @app.on_callback_query()
-async def translate_text(bot,update):
-  keybord6 =  InlineKeyboardMarkup([
-       [InlineKeyboardButton("Tayca",callback_data = "th"),
-       InlineKeyboardButton("Afrikanca", callback_data="af"),
-       InlineKeyboardButton("Türkməncə",callback_data ="tk")     
-       ],
-       [InlineKeyboardButton("Esperantoca",callback_data = "eo"),
-       InlineKeyboardButton("Urdu dili",callback_data = "ur"),
-       InlineKeyboardButton("Uyğurca",callback_data ="ug")
-       
-       ],
-       [InlineKeyboardButton("Özbəkcə",callback_data = "uz"),
-       InlineKeyboardButton("Vyetnam dili",callback_data ="vi"),
-       InlineKeyboardButton("Qaller dili",callback_data = "cy")
-       
-       ],
-       [InlineKeyboardButton("Xosa dili",callback_data = "xh"),
-       InlineKeyboardButton("Yəhudi dili",callback_data = "yi"),
-       InlineKeyboardButton("Yoruba dili",callback_data = "yo")
-       ],
-       [InlineKeyboardButton("Burada sizin reklamınız ola bilər ✔" ,url="https://t.me/vusalxw")
-       ],
-       [InlineKeyboardButton(" ««« Geri",callback_data = "page5")
-       
-       ]
- ])
-  
-  keybord5 = InlineKeyboardMarkup([
-         [InlineKeyboardButton("Şotlandca",callback_data = "gd"),
-         InlineKeyboardButton("Serbcə",callback_data = "sr"),
-         InlineKeyboardButton("Şesoto dili",callback_data = "st")
-         ],
-         [InlineKeyboardButton("Şona dili",callback_data ="sn"),
-         InlineKeyboardButton("Sindcə",callback_data ="sd"),
-         InlineKeyboardButton("Sinhala dili",callback_data = "si")
-         ],
-         [InlineKeyboardButton("Slovakca",callback_data = "sk"),
-         InlineKeyboardButton("Slovenya dili",callback_data = "sl"),
-         InlineKeyboardButton("Somali dili",callback_data = "so")
-         ],
-         [InlineKeyboardButton("Katalanca",callback_data = "ca"),
-         InlineKeyboardButton("Sundan dili",callback_data ="su"),
-         InlineKeyboardButton("Suahili dili",callback_data ="sw")
-         ],
-         [InlineKeyboardButton("İsveç dili",callback_data = "sv"),
-         InlineKeyboardButton("Filipin dili",callback_data ='tl'),
-         InlineKeyboardButton("Tacikcə",callback_data = "tg")
-         ],
-         [InlineKeyboardButton("Tamil dili",callback_data = "ta"),
-         InlineKeyboardButton("Tatarca",callback_data = "tt"),
-         InlineKeyboardButton("Telugu dili",callback_data = "te")
-         ],
-         [InlineKeyboardButton(" ««« Geri",callback_data = "page4"),
-         InlineKeyboardButton(" Növbəti »»»",callback_data = "page6")
-         ]  ])
-   
- 
-  keybord4 = InlineKeyboardMarkup([
-          [InlineKeyboardButton("Malayya dili",callback_data = "ml"),
-          InlineKeyboardButton("Malta dili",callback_data = "mt"),
-          InlineKeyboardButton("Maori dili",callback_data = "mi")
-          ],
-          [InlineKeyboardButton("Marathi dili",callback_data = "mr"),
-          InlineKeyboardButton("Monqolca",callback_data = "mn"),
-          InlineKeyboardButton("Myamma dili",callback_data = "my")
-          ],
-          [InlineKeyboardButton("Nepalca",callback_data ="ne"),
-          InlineKeyboardButton("Norveç dili",callback_data = "no"),
-          InlineKeyboardButton("Nyanja dili",callback_data = "ny")
-          ],
-          [InlineKeyboardButton("Odia dili",callback_data = "or"),
-          InlineKeyboardButton("Puştu dili ",callback_data = "ps"),
-          InlineKeyboardButton("Korsikanca",callback_data = "co"),
-          ],
-          [InlineKeyboardButton("Polyakca",callback_data = "pl"),
-          InlineKeyboardButton("Bosnakca",callback_data = "bs"),
-          InlineKeyboardButton("Pəncab dili",callback_data = "pa"),
-          ],
-          [InlineKeyboardButton("Romanca",callback_data = "ro"),
-          InlineKeyboardButton("Benqalca",callback_data = "bn"),
-          InlineKeyboardButton("Samoa dili",callback_data= "sm"),
-          ],
-          [InlineKeyboardButton(" ««« Geri",callback_data = "page3"),
-          InlineKeyboardButton(" Növbəti »»»",callback_data = "page5")
-          ]
-          
- 
- 
- 
- ])
-  
-  
-  keybord3 = InlineKeyboardMarkup([
-                [ InlineKeyboardButton("Hollandca",callback_data = "nl"),
-                InlineKeyboardButton("Yaponca",callback_data = "ja"),
-                InlineKeyboardButton("Yava dili",callback_data = "jv")
-                ],
-                [InlineKeyboardButton("Kanada dili",callback_data = "kn"),
-                InlineKeyboardButton("Qazax dili",callback_data = "kk"),
-                InlineKeyboardButton("Kxmer dili",callback_data = "km")
-                ],
-                [InlineKeyboardButton("Kinyarvan dili",callback_data = "rw"),
-                InlineKeyboardButton("Ərəbcə",callback_data ="ar"),
-                InlineKeyboardButton("Kürdcə",callback_data = "ku")
-                ],
-                [ InlineKeyboardButton("Qırğızca",callback_data ="ky"),
-                InlineKeyboardButton("Lao dili",callback_data = "lo"),
-                InlineKeyboardButton("Latınca",callback_data = "la")
-                ],
-                [InlineKeyboardButton("Latviyanca",callback_data = "lv"),
-                InlineKeyboardButton('Litvaca',callback_data ="lt"),
-                InlineKeyboardButton("Lüksemburq dili",callback_data = "lb")
-                ],
-                [InlineKeyboardButton("Makedonca",callback_data = "mk"),
-                InlineKeyboardButton("Malaqas dili",callback_data ="mg"),
-                InlineKeyboardButton("Malay dili",callback_data ="ms")
-                ],
-                [InlineKeyboardButton(" ««« Geri",callback_data = "page2"),
-                InlineKeyboardButton(" Növbəti »»»",callback_data = "page4")
-                ]
-              
- 
- ])
-  
-  
-  keybord1= InlineKeyboardMarkup( [
-        [ 
-            InlineKeyboardButton("Azərbaycanca",callback_data = 'az'),
-             InlineKeyboardButton("Almanca", callback_data='de'),
-            InlineKeyboardButton("Fransızca",callback_data ='fr'),
-        ],
-        [   InlineKeyboardButton("Koreya dili", callback_data='ko'),
-        InlineKeyboardButton("İngilscə", callback_data='en'),      
-        InlineKeyboardButton("Türkcə",callback_data = 'tr'),        
-        ],
-        [InlineKeyboardButton("Yunanca",callback_data ="el"),
-        	 InlineKeyboardButton("Belarusca",callback_data ="be"),       	
-	InlineKeyboardButton("Rusca",callback_data="ru")],
-	
-	[InlineKeyboardButton("Portuqalca",callback_data = "pt"),
-	InlineKeyboardButton("Bolqarca",callback_data ="bg"),
-	InlineKeyboardButton("İspanca",callback_data = "es")
-	],
-	[ 
-	InlineKeyboardButton("Farsca",callback_data ="fa"),
-	InlineKeyboardButton("Xorvatca",callback_data = "hr"),
-	InlineKeyboardButton("Çexcə", callback_data = "cs"),
-	],
-	[ InlineKeyboardButton("Danimarkaca",callback_data = "da"),
-	InlineKeyboardButton("İtalyanca",callback_data = "it"),
-	InlineKeyboardButton("Ukraynaca",callback_data = "uk"),	 
-	],
-        [InlineKeyboardButton("Burada sizin reklamınız ola bilər ✔" ,url="https://t.me/vusalxw")
-        ],
-	[InlineKeyboardButton(" Növbəti »»»",callback_data = "page2")
-	]
-	] )
-  
-  
-  keybord2= InlineKeyboardMarkup([
-           [InlineKeyboardButton("Ermənicə",callback_data = "hy"),
-           InlineKeyboardButton("Eston dili",callback_data = "et"),
-           InlineKeyboardButton("Finlandca",callback_data = "fi")
-           ],
-           [InlineKeyboardButton("Amharca",callback_data = "am"),
-           InlineKeyboardButton("Friz dili",callback_data = "fy"),
-           InlineKeyboardButton("Qalisiya dili",callback_data = "gl")
-           ],
-           [InlineKeyboardButton("Gürcü dili",callback_data = "ka"),
-           InlineKeyboardButton("Albanca",callback_data = "sq"),
-           InlineKeyboardButton("Baskca",callback_data = "em")
-           ],
-           [InlineKeyboardButton("Qucarati dili",callback_data = "gu"),
-           InlineKeyboardButton("Haiti dili",callback_data = "ht"),
-           InlineKeyboardButton("Hausa dili",callback_data ="ha")
-           ],
-           [InlineKeyboardButton("Hindicə",callback_data = "hi"),
-           InlineKeyboardButton("Macar dili",callback_data = "hu"),
-           InlineKeyboardButton("İslandiya dili",callback_data = "is")
-           ],
-           [InlineKeyboardButton("Iqboca",callback_data = "ig"),
-           InlineKeyboardButton("İndoneziya dili",callback_data = "id"),
-           InlineKeyboardButton("Irland dili",callback_data = "ga")
-           ],
-           [InlineKeyboardButton(" ««« Geri",callback_data = "page1"),
-           InlineKeyboardButton(" Növbəti »»»",callback_data = "page3"),
-           ]
-            ])
-						
-  
-  
-  
-from pyrogram import Client, filters
-from googletrans import Translator
+async def handle_callback(client, callback_query):
+    data = callback_query.data
+    message = callback_query.message
+    reply_text = message.reply_to_message.text if message.reply_to_message else ""
 
-
-
-# Define your asynchronous function
-async def my_function(update):
-    tr_text = update.message.reply_to_message.text if update.message.reply_to_message else ""
-    cb_data = update.data
-
-    if cb_data == "page2":
-        await update.message.edit("Dili seç ☟", reply_markup=keybord2)
-    elif cb_data == "page1":
-        await update.message.edit("Dili seç ☟", reply_markup=keybord1)
-    elif cb_data == "page3":
-        await update.message.edit("Dili seç ☟", reply_markup=keybord3)
-    elif cb_data == "page4":
-        await update.message.edit("Dili seç ☟", reply_markup=keybord4)
-    elif cb_data == "page5":
-        await update.message.edit("Dili seç ☟", reply_markup=keybord5)
-    elif cb_data == "page6":
-        await update.message.edit("Dili seç ☟", reply_markup=keybord6)
+    if data.startswith("page"):
+        await message.edit_text("Dili Seçin:", reply_markup=get_language_keyboard(data))
+    elif reply_text:
+        try:
+            translated = Translator().translate(reply_text, dest=data)
+            await message.edit_text(f"**Tərcümə:**\n{translated.text}")
+        except Exception as e:
+            await message.edit_text(f"Xəta baş verdi: {e}")
     else:
-        translator = Translator()
-        translation = translator.translate(tr_text, dest=cb_data)
-        await update.message.edit(translation.text)
+        await message.edit_text("Tərcümə üçün zəhmət olmasa bir mesaja cavab verin.")
 
-# Run your Pyrogram client
 app.run()
